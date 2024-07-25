dea3341aebf62150ba3d4f02a83946823741b1d8
syed Riyaz <76534599+riyazulahad786@users.noreply.github.com>
2024-07-26 01:00:48 +0530

    Delete pooling system directory

--- pooling system/Server/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import dotenv from 'dotenv';
-const app = express();
-const PORT = 3000;
-dotenv.config();
-app.listen(PORT,()=>{
-    console.log(`server in running at ${PORT}`)
-})
